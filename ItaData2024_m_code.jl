66b3be02ed8b8aec682b4161671add6582c67658
paso <paso.studio73@gmail.com>
2024-08-11 23:29:21 +0200

    print subfeatures names

--- ItaData2024_m_code.jl
+++ ItaData2024_m_code.jl
@@ -19,13 +19,13 @@ x, y = d["dataframe_validated"]
 close(d)
 
 # out of memory guard
-x = vcat(x[1:10, :], x[400:410, :])
-y = vcat(y[1:10], y[400:410])
+x = vcat(x[1:30, :], x[400:430, :])
+y = vcat(y[1:30], y[400:430])
 
 # cell 3 - Audio features extraction function
 nan_replacer!(x::AbstractArray{Float64}) = replace!(x, NaN => 0.0)
 
-function afe(x::AbstractVector{Float64})
+function afe(x::AbstractVector{Float64}; get_only_melfreq=false)
     # -------------------------------- parameters -------------------------------- #
     # audio module
     sr = 8000
@@ -78,6 +78,10 @@ function afe(x::AbstractVector{Float64})
         freq_range=freq_range
     );
 
+    if get_only_melfreq
+        return melfb.freq
+    end
+
     # mel spectrogram module
     melspec =  get_melspec(
         stft=stftspec, 
@@ -129,13 +133,22 @@ function afe(x::AbstractVector{Float64})
 end
 
 # cell 4 - Compute audio features dataframes and labels
-colnames = [
-    ["mel$i" for i in 1:26]...,
-    ["mfcc$i" for i in 1:13]...,
-    "f0", "cntrd", "crest", "entrp", "flatn", "flux", "kurts", "rllff", "skwns", "decrs", "slope", "sprd"
+color_code = Dict(:red => 31, :green => 32, :yellow => 33, :blue => 34, :magenta => 35, :cyan => 36)
+freq = round.(Int, afe(x[1, :audio]; get_only_melfreq=true))
+subfeats = [
+    ["\e[$(color_code[:yellow])mmel$i=$(freq[i])Hz\e[0m" for i in 1:26]...,
+    ["\e[$(color_code[:red])mmfcc$i\e[0m" for i in 1:13]...,
+    "\e[$(color_code[:green])mf0\e[0m", "\e[$(color_code[:cyan])mcntrd\e[0m", "\e[$(color_code[:cyan])mcrest\e[0m", 
+    "\e[$(color_code[:cyan])mentrp\e[0m", "\e[$(color_code[:cyan])mflatn\e[0m", "\e[$(color_code[:cyan])mflux\e[0m", 
+    "\e[$(color_code[:cyan])mkurts\e[0m", "\e[$(color_code[:cyan])mrllff\e[0m", "\e[$(color_code[:cyan])mskwns\e[0m", 
+    "\e[$(color_code[:cyan])mdecrs\e[0m", "\e[$(color_code[:cyan])mslope\e[0m", "\e[$(color_code[:cyan])msprd\e[0m"
 ]
 
-X = DataFrame([name => Vector{Float64}[] for name in colnames])
+features = [minimum, maximum]
+
+# feature_dict = Dict(i => "[$(col)-> $(func)]" for (i, (func, col)) in enumerate(Iterators.product(colnames, features)))
+
+X = DataFrame([name => Vector{Float64}[] for name in subfeats])
 
 for i in 1:nrow(x)
     push!(X, collect(eachcol(afe(x[i, :audio]))))
@@ -155,12 +168,12 @@ println("Test set size: ", size(X_test), " - ", length(y_test))
 
 # cell 6 - Train a model
 learned_dt_tree = begin
-    model = ModalDecisionTree(; relations = :IA7, features = [minimum, maximum])   
+    model = ModalDecisionTree(; relations = :IA7, features = features)   
     mach = machine(model, X_train, y_train) |> fit!
 end
 
 # BROKEN!
-report(learned_dt_tree).printmodel();
+# report(learned_dt_tree).printmodel();
 # BROKEN!
 # secondo me è perchè nell'interfaccia MLJ prima crea l'albero, che magari è multimodale,
 # mentre io verifico che sia unimodale solo quando lancia la funzione translate e
@@ -170,21 +183,27 @@ report(learned_dt_tree).printmodel();
 _, mtree = report(mach).sprinkle(X_test, y_test)
 sole_dt = ModalDecisionTrees.translate(mtree)
 
-printmodel(sole_dt; show_metrics = true);
+printmodel(sole_dt; show_metrics = true, subfeats=subfeats);
 
 # cell 8 - Extract rules that are at least as good as a random baseline model
 interesting_rules = listrules(sole_dt, min_lift = 1.0, min_ninstances = 0);
-printmodel.(interesting_rules; show_metrics = true);
+printmodel.(interesting_rules; show_metrics = true, subfeats=subfeats);
 
 # cell 9 - Simplify rules while extracting and prettify result
 interesting_rules = listrules(sole_dt, min_lift = 1.0, min_ninstances = 0, normalize = true);
-printmodel.(interesting_rules; show_metrics = true, syntaxstring_kwargs = (; threshold_digits = 2));
+printmodel.(interesting_rules; show_metrics = true, syntaxstring_kwargs = (; threshold_digits = 2), subfeats=subfeats);
 
 # cell 10 - Directly access rule metrics
 readmetrics.(listrules(sole_dt; min_lift=1.0, min_ninstances = 0))
 
 # cell 11 - Show rules with an additional metric (syntax height of the rule's antecedent)
-printmodel.(sort(interesting_rules, by = readmetrics); show_metrics = (; round_digits = nothing, additional_metrics = (; height = r->SoleLogics.height(antecedent(r)))));
+printmodel.(sort(interesting_rules, by = readmetrics); show_metrics = (; round_digits = nothing, additional_metrics = (; height = r->SoleLogics.height(antecedent(r)))), subfeats=subfeats);
 
 # cell 12 - Pretty table of rules and their metrics
-s1 = metricstable(interesting_rules; metrics_kwargs = (; round_digits = nothing, additional_metrics = (; height = r->SoleLogics.height(antecedent(r)))))
+subfeats = [
+    ["mel$i" for i in 1:26]...,
+    ["mfcc$i" for i in 1:13]...,
+    "f0", "cntrd", "crest", "entrp", "flatn", "flux", "kurts", "rllff", "skwns", "decrs", "slope", "sprd"
+]
+
+s1 = metricstable(interesting_rules; subfeats=subfeats, metrics_kwargs = (; round_digits = nothing, additional_metrics = (; height = r->SoleLogics.height(antecedent(r)))))
